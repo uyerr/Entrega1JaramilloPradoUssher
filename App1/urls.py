598efb59dcf25f6a4a2248b270b80fbf71f84949
Santy <santiago01prado@gmail.com>
2022-10-17 04:49:13 -0300

    Commit 2

--- App1/urls.py
+++ App1/urls.py
@@ -1,8 +1,8 @@
 from App1 import views
 from django.urls import path
-from importlib.resources import path
+
 
 
 urlpatterns = [
-    path('home/', views.home),   
+    path('home/', views.home)
 ]
