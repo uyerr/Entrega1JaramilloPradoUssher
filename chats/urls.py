45b2d3fc1a95bf674f24e374aa1fbf4c7077d476
uyerr <santiussher70@gmail.com>
2022-11-10 16:15:45 -0300

    route messages y admin.py actualizado

--- chats/urls.py
+++ chats/urls.py
@@ -2,8 +2,8 @@ from django.urls import include, path
 from .views import CreateThread, ListThreads, ThreadView, CreateMessage
 
 urlpatterns = [
-    path('inbox/', ListThreads.as_view(), name='inbox'),
-    path('inbox/create-thread/', CreateThread.as_view(), name='create_thread'),
-    path('inbox/<int:pk>/', ThreadView.as_view(), name='thread'),
-    path('inbox/<int:pk>/create-message', CreateMessage.as_view(), name='create_message'),
+    path('messages/', ListThreads.as_view(), name='inbox'),
+    path('messages/create-thread/', CreateThread.as_view(), name='create_thread'),
+    path('messages/<int:pk>/', ThreadView.as_view(), name='thread'),
+    path('messages/<int:pk>/create-message', CreateMessage.as_view(), name='create_message'),
 ]
